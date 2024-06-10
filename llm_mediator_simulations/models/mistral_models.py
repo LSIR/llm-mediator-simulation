e9f7a2cf9b91f2ca2e7b6859a289b416b3d02978
Thibaut de Saivre <thibaut.de-saivre@polytechnique.edu>
2024-06-10 18:22:59 +0200

    Add mistral models

---
+++ llm_mediator_simulations/models/mistral_models.py
@@ -0,0 +1,38 @@
+"""Mistral model wrapper."""
+
+from typing import Literal, override
+
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
+
+from llm_mediator_simulations.models.language_model import LanguageModel
+
+
+class MistralModel(LanguageModel):
+    """Mistral model wrapper."""
+
+    def __init__(
+        self,
+        *,
+        api_key: str,
+        model_name: Literal["mistral-large-latest", "mistral-small-latest"]
+    ):
+        """Initialize a Mistral model.
+
+        Args:
+            api_key: OpenAI API key.
+            model_name: OpenAI model name.
+        """
+
+        self.client = MistralClient(api_key=api_key)
+        self.model_name = model_name
+
+    @override
+    def sample(self, prompt: str) -> str:
+        """Generate text based on the given prompt."""
+
+        response = self.client.chat(
+            model=self.model_name, messages=[ChatMessage(role="user", content=prompt)]
+        )
+
+        return response.choices[0].message.content
